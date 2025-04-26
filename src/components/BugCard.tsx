71f21545b1a62a976c29215f75137d60f1a4cf31
JonathanYin <joyin@ucsd.edu>
2025-04-26 15:20:57 -0700

    Add more bugs

--- src/components/BugCard.tsx
+++ src/components/BugCard.tsx
@@ -6,13 +6,17 @@ import cockroachImg from "../assets/cockroach-min.png";
 import flyImg from "../assets/fly-min.png";
 import ladybugImg from "../assets/ladybug-min.png";
 import spiderImg from "../assets/spider-min.png";
+import antImg from "../assets/ant-min.png";
+import beetleImg from "../assets/beetle-min.png";
+import caterpillarImg from "../assets/caterpillar-min.png";
+import mothImg from "../assets/moth-min.png";
 
 interface Props {
 	bug: Bug;
 }
 
 // Array of all bug images
-const bugImages = [cockroachImg, flyImg, ladybugImg, spiderImg];
+const bugImages = [cockroachImg, flyImg, ladybugImg, spiderImg, antImg, beetleImg, caterpillarImg, mothImg];
 
 // Get a random bug image based on bug id
 const getBugImage = (id: string) => {
