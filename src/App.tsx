34c2810dc28256e7ad63228504068f5f48fd43bc
JonathanYin <joyin@ucsd.edu>
2025-04-26 15:49:52 -0700

    Style buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bugs from "./routes/Bugs";
 import Leaderboard from "./routes/Leaderboard";
 import UserProfile from "./routes/UserProfile";
+import { MainNav } from "./components/MainNav";
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<header className="sticky top-0 z-10 flex items-center justify-center gap-4 bg-white/80 py-4 shadow backdrop-blur">
-				<NavLink to="/" end className={({ isActive }) => `btn ${isActive ? "bg-emerald-600" : "bg-gray-400"}`}>
-					🐛 Bugs
-				</NavLink>
-				<NavLink to="/bounty-leaderboard" className={({ isActive }) => `btn ${isActive ? "bg-indigo-600" : "bg-gray-400"}`}>
-					🏆 Leaderboard
-				</NavLink>
-			</header>
+			<MainNav />
 
 			<Routes>
 				<Route path="/" element={<Bugs />} />
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import { cn } from "../lib/utils";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "./ui/navigation-menu";
+import { Button } from "./ui/button";
+
+export function MainNav() {
+	const location = useLocation();
+
+	return (
+		<div className="sticky top-0 z-10 flex items-center justify-center gap-4 bg-white/80 py-4 shadow backdrop-blur">
+			<NavigationMenu>
+				<NavigationMenuList>
+					<NavigationMenuItem>
+						<Link to="/">
+							<Button variant={location.pathname === "/" ? "emerald" : "gray"} className="w-32">
+								🐛 Bugs
+							</Button>
+						</Link>
+					</NavigationMenuItem>
+					<NavigationMenuItem>
+						<Link to="/bounty-leaderboard">
+							<Button variant={location.pathname === "/bounty-leaderboard" ? "indigo" : "gray"} className="w-32">
+								🏆 Leaderboard
+							</Button>
+						</Link>
+					</NavigationMenuItem>
+				</NavigationMenuList>
+			</NavigationMenu>
+		</div>
+	);
+}
